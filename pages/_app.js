ff7394dea02919344262b27b1a39253ff8e54d82
foxyuh <arthurbryansk@gmail.com>
2022-11-01 22:57:13 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "../styles/globals.css";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
