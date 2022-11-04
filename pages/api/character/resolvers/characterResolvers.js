07a91c94e44f1f2f3b084116e887cb155086060a
foxyuh <arthurbryansk@gmail.com>
2022-11-04 16:58:35 -0300

    arquivos deletados

--- pages/api/character/resolvers/characterResolvers.js
+++
@@ -1,9 +0,0 @@
-const characterResolvers = {
-    Query: {
-        users: (root, args, { dataSources }) => dataSources.CharactersAPI.getCharacters(),
-        user: (root, { id }, { dataSources }) => dataSources.CharactersAPI.getCharacterById(id)
-    }
-
-}
-
-export default characterResolvers
