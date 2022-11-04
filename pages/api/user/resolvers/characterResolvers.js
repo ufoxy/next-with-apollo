6f5cf362b187d860ee906b774c522e2bbf0d9852
foxyuh <arthurbryansk@gmail.com>
2022-11-04 17:01:31 -0300

    deleted archives

--- pages/api/user/resolvers/characterResolvers.js
+++
@@ -1,8 +0,0 @@
-const userResolvers = {
-  Query: {
-    users: (root, args, { dataSources }) => dataSources.usersAPI.getCharacters(),
-    user: (root, { id }, { dataSources }) => dataSources.usersAPI.getCharacterById(id)
-  }
-}
-
-module.exports = userResolvers
