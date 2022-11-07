c537a3f11a3bbccee8f0ff5c2e5c1c8f6f7a7090
foxyuh <arthurbryansk@gmail.com>
2022-11-06 22:12:06 -0300

    Update mutation.js

--- pages/api/character/typeDefs/mutation.js
+++ pages/api/character/typeDefs/mutation.js
@@ -1,26 +1,26 @@
 const { gql } = require("apollo-server");
 const mutation = gql`
-   type Mutation {
-      createCharacter(character: CharacterInput): Character
-      updateCharacter(id: String, character: CharacterInput): Character
-      deleteCharacter(id: String): Character
-   }
-   
-   input CharacterInput {
-    name: String
-      status: String
-      species: String
-      type: String
-      gender: String
-      origin: OriginInput
-      location: LocationInput
-      image: String
-      episode: [String]
-      url: String
-      created: DateTime
-   }
+  type Mutation {
+    createCharacter(character: CharacterInput): Character
+    updateCharacter(id: String, character: CharacterInput): Character
+    deleteCharacter(id: String): Character
+  }
 
-   input OriginInput {
+  input CharacterInput {
+    name: String
+    status: String
+    species: String
+    type: String
+    gender: String
+    origin: OriginInput
+    location: LocationInput
+    image: String
+    episode: [String]
+    url: String
+    created: DateTime
+  }
+
+  input OriginInput {
     name: String
     url: String
   }
@@ -29,6 +29,5 @@ const mutation = gql`
     name: String
     url: String
   }
-
 `;
 module.exports = mutation;
