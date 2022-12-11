956a227d67992869b3dbef1c07300c7e24797eac
Arthur Bryan <arthurbryansk@gmail.com>
2022-12-11 17:27:20 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
-const appRoute = require("./src/routes/app.route")
-const port = process.env.PORT || 3000
+const appRoute = require("./src/routes/app.route");
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/app", appRoute)
+app.use("/app", appRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Welcome to API!" });
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Express running on http://localhost:${port}`);
 });
