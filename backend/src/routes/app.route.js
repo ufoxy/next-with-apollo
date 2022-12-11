0f9ad2993615f520782649e2f42af9e7efa44890
Arthur Bryan <arthurbryansk@gmail.com>
2022-12-11 17:27:29 -0300

    Update app.route.js

--- backend/src/routes/app.route.js
+++ backend/src/routes/app.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const appController = require("../controllers/app.controller");
 
-router.get("/characters", appController.getCharacters);
+// router.get("/characters", appController.getCharacters);
+router.get("/characters/:page", appController.getCharactersPerPage);
+router.get("/character/:id", appController.getCharacterById);
 
-module.exports = router
+module.exports = router;
