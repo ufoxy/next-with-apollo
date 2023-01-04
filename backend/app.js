f5f297b5b8e205543725b180c92ccd45e711508c
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-04 19:05:52 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const appRoute = require("./src/routes/app.route");
+const port = 8000
 
 const app = express();
 
@@ -12,6 +13,6 @@ app.get("/", (req, res) => {
   return res.status(200).json({ msg: "Welcome to API!" });
 });
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log(`Express running on http://localhost:${port}`);
 });
