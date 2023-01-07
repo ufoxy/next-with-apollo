9749d400b5e9d6d7743aad32fc3d4b3d7867301e
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-06 21:59:34 -0300

    Update card.jsx

--- frontend/components/card.jsx
+++ frontend/components/card.jsx
@@ -8,34 +8,38 @@ function Card({ name, image, status, specie }) {
     let nm = name.split(" ").shift();
     const abv = name.split(" ").filter((e) => (e == nm ? false : true));
     abv.map((e) => (nm += ` ${e.substr(0, 1)}.`));
-    name = nm;
+    nm.length > 19 ? (name = `${nm.substr(0, 19)} ...`) : (name = nm);
+    // name = nm;
   } // Abrevia nomes grandes
 
   return (
     <React.Fragment>
-      <article className={styles.article}>
-        <div className={styles.image_div}>
-          <Image
-            src={image}
-            width="280"
-            height="280"
-            priority={true}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.info_container}>
-          <h2>{name}</h2>
-          <div className={styles.flex}>
-            <MdCircle
-              color={status === "Alive" ? "green" : "red"}
-              className={styles.circle}
+      <div className={styles.article_div}>
+        <article className={styles.article}>
+          <div className={styles.image_div}>
+            <Image
+              src={image}
+              width="280"
+              height="280"
+              priority={true}
+              className={styles.image}
             />
-            <p>
-              {status} - {specie}
-            </p>
           </div>
-        </div>
-      </article>
+          <div className={styles.info_container}>
+            <h2>{name}</h2>
+            <div className={styles.flex}>
+              <MdCircle
+                color={status === "Alive" ? "green" : "red"}
+                className={styles.circle}
+              />
+              <p>
+                {status === "unknown" ? "UNK" : status} -{" "}
+                {specie === "unknown" ? "UNK" : specie}
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
     </React.Fragment>
   );
 }
