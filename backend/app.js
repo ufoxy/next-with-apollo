424b5afc443246b359ecdf807c0611a223dad078
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-06 21:59:27 -0300

    instalação e uso do cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const appRoute = require("./src/routes/app.route");
-const port = 8000
+const port = 8000;
 
 const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.use(express.json());
 
