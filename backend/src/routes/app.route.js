bed522a5e37ebb63284522011922af2f63bce303
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-22 19:48:48 -0300

    Update app.route.js

--- backend/src/routes/app.route.js
+++ backend/src/routes/app.route.js
@@ -4,6 +4,12 @@ const appController = require("../controllers/app.controller");
 
 // router.get("/characters", appController.getCharacters);
 router.get("/characters/:page", appController.getCharactersPerPage);
-router.get("/character/:id", appController.getCharacterById);
+router.get("/character/id/:id", appController.getCharacterById);
+router.get("/character/name/:name", appController.getCharacterByName);
+router.get("/random/characters", appController.getRandomCharacters);
+
+// -------
+
+// router.get("/organize/characters/:by", appController.getCharacterByName);
 
 module.exports = router;
