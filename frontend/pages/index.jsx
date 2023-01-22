78150591e8f80573415241a1a7f6d7a4986b25a4
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-22 19:52:47 -0300

    Update index.jsx

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -33,7 +33,7 @@ export async function getStaticProps() {
 export default function Home({ character }) {
   const [characters, setCharacters] = useState(character.characters);
   const [autoLoad, setAutoLoad] = useState(false)
-  let fetchDataTimeout = 0
+  let fetchDataTimeout = 2000
 
   function autoLoadFunction() {
     if(autoLoad === false) {
@@ -85,7 +85,6 @@ export default function Home({ character }) {
       const newPosts = await res.json();
       console.log(newPosts)
       setCharacters((character) => [...characters, ...newPosts.characters]);
-      fetchDataTimeout = 2000
     }, fetchDataTimeout);
   };
 
