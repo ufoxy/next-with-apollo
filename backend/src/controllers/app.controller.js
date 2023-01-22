44179411e08a1bc132bf4e48e3e33797a873630a
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-22 19:48:45 -0300

    Update app.controller.js

--- backend/src/controllers/app.controller.js
+++ backend/src/controllers/app.controller.js
@@ -13,6 +13,24 @@ async function getCharacters(req, res) {
   res.status(200).json({ characters });
 }
 
+async function getRandomCharacters(req, res) {
+  const collection = await getCollection(clientPromise, "app", "characters");
+
+  const page = Math.floor(Math.random() * (92 - 1) + 1);
+  const limit = 9;
+  const skip = page * limit;
+
+  const characters = await collection
+    .find({}, { skip: skip, limit: limit })
+    .toArray();
+
+  if (!characters) {
+    return res.status(404).json({ msg: "Lista de personagens indisponível!" });
+  }
+
+  res.status(200).json({ characters });
+}
+
 async function getCharactersPerPage(req, res) {
   let pagination = await req.params.page;
 
@@ -47,8 +65,26 @@ async function getCharacterById(req, res) {
   res.status(200).json({ character });
 }
 
+async function getCharacterByName(req, res) {
+  let name = String(await req.params.name);
+  console.log(name);
+  const reg = new RegExp(`^${name}`, "i");
+
+  const collection = await getCollection(clientPromise, "app", "characters");
+  const character = await collection.find({ name: { $regex: reg } }).toArray();
+  console.log(character);
+
+  if (!character) {
+    return res.status(404).json({ msg: "Personagem não encontrado!" });
+  }
+
+  res.status(200).json({ character });
+}
+
 module.exports = {
   getCharacters,
+  getRandomCharacters,
   getCharactersPerPage,
   getCharacterById,
+  getCharacterByName,
 };
