841d92f2574c731509fa06f2078f5591a0c79d14
Arthur Bryan <arthurbryansk@gmail.com>
2023-01-25 16:36:05 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,11 @@ const app = express();
 const cors = require("cors");
 const corsOptions = {
   origin: "*",
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
